7c59787ea23e330b443049e5d9ddcb56c332d7a8
AKonicki26 <anne.konicki@mymail.champlain.edu>
2025-04-06 18:20:05 -0400

    Add comment

--- chess-cli/main.cpp
+++ chess-cli/main.cpp
@@ -13,6 +13,7 @@ int main() {
     for (auto move : board.getValidMoves(board.getCurrentColor()))
         std::cout << move << "\n";
 
+    // Fen board notation for here stolen from chess.com fen explanation
     board = Board("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1");
     std::cout << "\n";
     board.printBoard();
