71a09a21c74a20fd64a4c8dd97760a66cc62cb7c
AKonicki26 <anne.konicki@mymail.champlain.edu>
2025-04-06 18:19:24 -0400

    Add another example to console testing

--- chess-cli/main.cpp
+++ chess-cli/main.cpp
@@ -7,13 +7,17 @@
 
 int main() {
     Board board;
-    auto moves = board.getValidMoves(board.getCurrentColor());
-
     board.printBoard();
-
     std::cout << "\n";
-
-    for (auto move : moves) {
+    
+    for (auto move : board.getValidMoves(board.getCurrentColor()))
         std::cout << move << "\n";
-    }
+
+    board = Board("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1");
+    std::cout << "\n";
+    board.printBoard();
+    std::cout << "\n";
+    
+    for (auto move : board.getValidMoves(board.getCurrentColor()))
+        std::cout << move << "\n";
 }
